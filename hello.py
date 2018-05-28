0caaf36bd9e7961a3df047aaa92fbdbb9796ff9e
keishi0326 <38975942+keishi0326@users.noreply.github.com>
2018-05-28 15:29:27 +0900

    Update hello.py

--- hello.py
+++ hello.py
@@ -418,7 +418,55 @@ def hello_world():
 	joblib.dump(clf_cv, 'data/model_v2.pkl', compress=True)
 	
 	return "recommendation model created!!"
+
+@route("/createmodel_s3upload")
+def hello_world():
+	df = pd.read_csv('data/promotion.csv',sep=',',encoding='SHIFT-JIS', parse_dates = [2,3])
+
+	df = common.transform(df)
 	
+	print(df)
+
+	data = df.drop(["Time", "performance", "date"], axis=1)
+
+	#target = df['y']
+	target = df['performance']
+
+	data_train_s, data_test_s, label_train_s, label_test_s = cross_validation.train_test_split(data, target, test_size=0.01)
+	parameters = {
+              'n_estimators' : [100, 500],                 
+              'learning_rate' : [0.1], 
+              'max_depth': [4],
+              'min_samples_leaf': [9],
+              'max_features': [1.0, 0.3]
+              }
+
+	clf_cv = grid_search.GridSearchCV(GradientBoostingRegressor(), parameters, cv = 4, scoring='neg_mean_absolute_error')
+
+	clf_cv.fit(data_train_s, label_train_s)
+
+	print("Best Model Parameter: ", clf_cv.best_params_)
+	print("Best Model Score: ", clf_cv.best_score_)
+
+	file_name = "data/model_temp.pkl"
+
+	# 学習した分類器を保存する。
+	joblib.dump(clf_cv, file_name, compress=True)
+
+	print("Model save process normally end.")
+	
+	S3_BUCKET = os.environ.get('S3_BUCKET')
+
+	file_type = "application/zip"
+
+	s3 = boto3.resource('s3')
+	
+	# s3へのファイルアップロード
+	s3.meta.client.upload_file(file_name, S3_BUCKET, 'model.pkl')
+	
+	return "recommendation model created and S3 upload succeeded!!"
+
+
 #  キャンペーン候補追加
 def insert_campaign(conn, cur, storeID, campaign_df, result):
 	cur.execute("SELECT campaigncandidateid__c FROM salesforce.CampaignCandidate__c order by campaigncandidateid__c desc")
